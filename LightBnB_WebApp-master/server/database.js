0d237576e7a9dfb50667a25d25dbf8ee87b33ebe
Aaron <aaronpiolang@gmail.com>
2020-01-09 20:56:46 +0000

    a little code organizing. Still need to restructure

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -11,12 +11,8 @@ const pool = new Pool({
 });
 
 /// Users
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-/**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
 const getUserWithEmail = function(email) {
   const queryString = `
   SELECT * FROM users
@@ -27,11 +23,8 @@ const getUserWithEmail = function(email) {
 };
 exports.getUserWithEmail = getUserWithEmail;
 
-/**
- * Get a single user from the database given their id.
- * @param {string} id The id of the user.
- * @return {Promise<{}>} A promise to the user.
- */
+//------------------------------------------------------------------
+
 const getUserWithId = function(id) {
   const queryString = `
   SELECT * FROM users
@@ -42,11 +35,8 @@ const getUserWithId = function(id) {
 };
 exports.getUserWithId = getUserWithId;
 
-/**
- * Add a new user to the database.
- * @param {{name: string, password: string, email: string}} user
- * @return {Promise<{}>} A promise to the user.
- */
+//------------------------------------------------------------------
+
 const addUser = function(user) {
   const queryString = `
   INSERT INTO users (name, password, email) 
@@ -59,12 +49,8 @@ const addUser = function(user) {
 exports.addUser = addUser;
 
 /// Reservations
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-/**
- * Get all reservations for a single user.
- * @param {string} guest_id The id of the user.
- * @return {Promise<[{}]>} A promise to the reservations.
- */
 const getAllReservations = function(guest_id, limit = 10) {
   const queryString = `
   SELECT *, cover_photo_url
@@ -79,13 +65,7 @@ const getAllReservations = function(guest_id, limit = 10) {
 exports.getAllReservations = getAllReservations;
 
 /// Properties
-
-/**
- * Get all properties.
- * @param {{}} options An object containing query options.
- * @param {*} limit The number of results to return.
- * @return {Promise<[{}]>}  A promise to the properties.
- */
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 const getAllProperties = function(options, limit = 10) {
   let queryParams = [];
@@ -142,11 +122,7 @@ const getAllProperties = function(options, limit = 10) {
 };
 exports.getAllProperties = getAllProperties;
 
-/**
- * Add a property to the database
- * @param {{}} property An object containing all of the property details.
- * @return {Promise<{}>} A promise to the property.
- */
+//------------------------------------------------------------------
 const addProperty = function(property) {
   const queryString = `
   INSERT INTO properties (  owner_id,
